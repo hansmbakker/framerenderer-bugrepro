7556c26ed34c4adc4ae62c6fec42367f475f9b2a
Hans Bakker <hansmbakker@hotmail.com>
2024-01-08 18:59:55 +0100

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            MainPage = new AppShell();
+        }
+
+        private void CurrentDomain_FirstChanceException(object? sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            Debug.WriteLine($"*** UNHANDLED EXCEPTION: {e.Exception}");
+            Debugger.Break();
+        }
+    }
+}
